7f98ec4d999f3625be0a41c0b9d2185da3981d0d
Andrey Belskiy <diksandab@gmail.com>
2021-04-24 15:20:54 +0600

    3 task complete

--- tasks/addition/reader.sql
+++ tasks/addition/reader.sql
@@ -1,6 +1,15 @@
+-- Читатель по количеству
 SELECT reader_id, CONCAT(first_name, ' ', last_name), COUNT(*)
 FROM readers
 JOIN library_queue USING(reader_id)
 GROUP BY reader_id
 ORDER BY COUNT(*) DESC
-LIMIT 1
+LIMIT 1;
+
+-- По времени чтения
+SELECT reader_id, CONCAT(first_name, ' ', last_name), COUNT(*) as books_quantity, AVG(taken - given) AS reading_time
+FROM readers
+JOIN library_queue USING(reader_id)
+GROUP BY reader_id
+ORDER BY reading_time DESC
+LIMIT 1;
--- tasks/task_3.md
+++ tasks/task_3.md
@@ -16,6 +16,11 @@ LIMIT 1
 2. Долго не возвращает книги (по среднему времени задержки книги)
 
 ```mysql
-
+SELECT reader_id, CONCAT(first_name, ' ', last_name), COUNT(*) as books_quantity, AVG(taken - given) AS reading_time
+FROM readers
+JOIN library_queue USING(reader_id)
+GROUP BY reader_id
+ORDER BY reading_time DESC
+LIMIT 1;
 ```
 
